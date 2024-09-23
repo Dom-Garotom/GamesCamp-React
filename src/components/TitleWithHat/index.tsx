2918fdde60c5a8ee586e0d8029321653420c6414
Gean ferrera <145035724+Dom-Garotom@users.noreply.github.com>
2024-09-23 14:20:28 -0300

    feat: criando o componente de um titulo com chapéu

---
+++ src/components/TitleWithHat/index.tsx
@@ -0,0 +1,35 @@
+import styled from "styled-components";
+import { TitleDefault } from "../../styles/styledComponents";
+
+
+const WrapperTitle = styled.div`
+    margin: 18px 0px;
+    position: relative;
+    span{
+        position: absolute;
+        font-weight: 500;
+        color: #fff;
+        top: -18px;
+        font-size: 1.2rem;
+    }
+
+    h2{
+        font-size: 3.7rem;
+    }
+
+`
+
+type PropsTitleWithHat ={
+    content : string,
+    hat : string,
+}
+
+
+export default function TitleWithHat(props : PropsTitleWithHat ) {
+  return (
+   <WrapperTitle>
+    <span>{props.hat}</span>
+    <TitleDefault>{props.content}</TitleDefault>
+   </WrapperTitle>
+  )
+}
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,13 +1,16 @@
 import { TitleDefault , MainTitle , ButtonDefault , ButtonCTA , ButtonExit} from "../styles/styledComponents"
+import TitleWithHat from "../components/TitleWithHat"
 
 export default function Home() {
   return (
     <>
-    <MainTitle>Games Camp 2024</MainTitle>
+      <MainTitle>Games Camp 2024</MainTitle>
       <TitleDefault>Teste</TitleDefault>
+      <TitleWithHat content="Games Camp" hat="Melhor campeonato do pais" />
       <ButtonDefault>Inscreva-se</ButtonDefault>
       <ButtonCTA>Preencher o formulario</ButtonCTA>
       <ButtonExit>X</ButtonExit>
+
     </>
   )
 }
