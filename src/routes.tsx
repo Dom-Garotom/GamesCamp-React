c02da8fb201df6647d5373a1606cd4b28172a151
Gean ferrera <145035724+Dom-Garotom@users.noreply.github.com>
2024-09-26 13:13:03 -0300

    refactor: ajustando as rotas

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,12 +1,13 @@
 import { Route, BrowserRouter, Routes as RouterRoutes } from "react-router-dom"
 import Home from "./pages/home"
+import Inscricao from "./pages/inscriçao"
 
 export default function Routes() {
     return (
         <BrowserRouter>
             <RouterRoutes>
                 <Route path="/" element={<Home />} />
-                <Route path="/inscrição" element={<Home />} />
+                <Route path="/inscrição" element={<Inscricao />} />
             </RouterRoutes>
         </BrowserRouter>
     )
